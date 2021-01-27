e00ff04458989cd36ab40e8fb95c875d8cce2036
willis240 <willingness240@gmail.com>
2021-01-26 16:10:28 -0900

    initial commit

---
+++ AI_Tic_Tac_Toe/header.hpp
@@ -0,0 +1,10 @@
+// header.hpp
+// William Fisher
+// Jan. 26, 2021
+
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+
+
+#endif
---
+++ AI_Tic_Tac_Toe/source.cpp
@@ -0,0 +1,8 @@
+// source.cpp
+// William Fisher
+// Jan. 26, 2021
+
+int main()
+{
+
+}
