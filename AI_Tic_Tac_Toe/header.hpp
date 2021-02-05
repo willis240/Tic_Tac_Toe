ebd081ba6782627fd9b0738854b027f5812e5c6f
willis240 <willingness240@gmail.com>
2021-02-04 17:23:13 -0900

    reorganized code to be more presentable

--- AI_Tic_Tac_Toe/header.hpp
+++ AI_Tic_Tac_Toe/header.hpp
@@ -10,5 +10,11 @@
 #include <sstream>
 #include <string>
 
+void placeMarker(int slot, std::vector<int>& board);
+void drawBoard(std::vector<int>& board);
+void playerTurn(int playerNum, std::vector<int>& board);
+bool getResult(const std::vector<int>& check);
+bool tieCheck(std::vector<int>& board);
+bool winCheck(std::vector<int>& board);
 
 #endif
